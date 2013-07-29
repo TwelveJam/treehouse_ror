170052727789ded1a4e47a2c81d783f113ef242d
Kamal Leadbitter <k.leadbitter@me.com>
2013-07-29 22:48:45 +0100

    My first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twelvejam::Application.config.secret_token = 'd99d16a2e50e4f3e67e567f12e81bb154457fcd9472d371a8d48516ce77c00bfc3fe8203908f0c5a954755aa346d4321930bc9f211d751b3bd200dd24484dcad'
